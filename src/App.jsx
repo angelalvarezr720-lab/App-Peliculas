aaf029971a3a50af968831fd07977bd352116602
laalvarez <luisangel.alvarez@segurosuniversitas.com>
2026-08-17 16:11:39 -0400

    nada

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='container-fluid'>
-        <h1>Luisss</h1>
-      </div>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
