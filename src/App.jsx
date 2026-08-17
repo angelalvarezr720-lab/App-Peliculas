430fbcec200f55c5d82e4b186c8f6c850df153a2
laalvarez <luisangel.alvarez@segurosuniversitas.com>
2026-08-17 15:56:19 -0400

    Initial commit - Buscador de peliculas

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container-fluid'>
+        <h1>Luisss</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
